0c9532b251840fd8134583757c6222741c32f3b1
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-26 04:17:37 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -688,7 +688,7 @@ async def lol_cb(b, cb):
                 ],
             ]
     )
-    await generate_cover(title, views, duration, thumbnail)
+    await generate_cover(title, views, duration)
     file_path = await converter.convert(youtube.download(url))  
     if chat_id in callsmusic.pytgcalls.active_calls:
         position = await queues.put(chat_id, file=file_path)
