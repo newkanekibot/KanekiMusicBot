f183be3e0d386e994cb407117ab2b3b48a01cd03
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-26 15:46:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/49ac0c33e0f9466934427.jpg")
 THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/49ac0c33e0f9466934427.jpg")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/49ac0c33e0f9466934427.jpg")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
