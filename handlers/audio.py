1d34f4a4ac734b0b6dd80984e7622391a36eb660
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-30 01:32:50 +0700

    kaneki

--- handlers/audio.py
+++ handlers/audio.py
@@ -3,7 +3,7 @@ from os import path
 from pyrogram import Client
 from pyrogram.types import Message, Voice
 from pytgcalls.types.input_stream import InputAudioStream
-from Client import callsmusic, queues
+from callsmusic import callsmusic, queues
 
 import converter
 from youtube import youtube
